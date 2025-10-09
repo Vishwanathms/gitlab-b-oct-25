e47b5d5eb190b0df89ee509ca0ff1d515f0e5103
Vishwanathms <vishwanath.murthy@gmail.com>
2025-10-09 11:13:17 +0530

    rectified

---
+++ D4/tf-codes/ex05-vm-creation/dev.tfvars
@@ -0,0 +1,4 @@
+vm-name = "terraform-vm1-dev"
+ip-add = "172.16.101.53"
+vcenter_user = "administrator@vsphere.local"
+vcenter_password = "VMware1!VMware1!"
---
+++ D4/tf-codes/ex05-vm-creation/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.7.0"
+    }
+  }
+}
+
+provider "vsphere" {
+  # user           = "administrator@vsphere.local"
+  # password       = "VMware1!VMware1!"
+  user = var.vcenter_user
+  password = var.vcenter_password
+  vsphere_server = "vcenter01.vishwacloudlab.in"
+
+  # Disable SSL verify if using self-signed certificate
+  allow_unverified_ssl = true
+}
+
+terraform {
+  backend "s3" {
+    bucket = "vishwa11032025"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    # access_key = var.access_key
+    # secret_key = var.secret_key
+
+    # Uncomment below if you use DynamoDB for state locking
+    # dynamodb_table = "your-lock-table"
+    # encrypt = true
+  }
+}
