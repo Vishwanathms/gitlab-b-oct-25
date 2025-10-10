6e86f68bf5ee7695cefcbd6deed5f051812e9e43
Vishwanathms <vishwanath.murthy@gmail.com>
2025-10-10 11:18:09 +0530

    d7 docs updated

---
+++ D7/dc-ex02/app.py
@@ -0,0 +1,14 @@
+import os
+import redis
+import time
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
+r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+while True:
+    r.set("test_key", "Hello from Python + Redis")
+    value = r.get("test_key")
+    print(f"Redis says: {value}")
+    time.sleep(3)
