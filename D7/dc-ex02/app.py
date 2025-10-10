c2daea05c005825ce40e2c82c398ea9df32cb9e7
Vishwanathms <vishwanath.murthy@gmail.com>
2025-10-10 11:35:49 +0530

    d7 docs updated

--- D7/dc-ex02/app.py
+++ D7/dc-ex02/app.py
@@ -1,14 +1,19 @@
+from flask import Flask
 import os
 import redis
-import time
+
+app = Flask(__name__)
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
-while True:
-    r.set("test_key", "Hello from Python + Redis")
-    value = r.get("test_key")
-    print(f"Redis says: {value}")
-    time.sleep(3)
+@app.route('/')
+def index():
+    r.incr('hits')
+    count = r.get('hits')
+    return f"Hello! This page has been viewed {count} times."
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
